e886a5354c8316024192a8494a3ead73dc723edc
unknown <you@example.com>
2026-08-24 15:55:10 +0530

    Updated

---
+++ JavaScript/Day5/assets/JS/Task8.js
@@ -0,0 +1,5 @@
+for (let o = 1; o <= 20; o++) {
+    if (o % 2 !== 0) {
+        console.log(o);
+    }
+}
---
+++ JavaScript/Looping statements/assets/js/index.js
@@ -0,0 +1,45 @@
+let n = 5;
+let factorial = 1;
+
+for (let i = 1; i <= n; i++) {
+    factorial = factorial * i;
+}
+
+console.log(factorial);
+
+
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+let sum = a + b + c;
+
+console.log(sum);
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+let average = (a + b + c) / 3;
+
+console.log(average);
+
+let number = 5;
+
+let square = number * number;
+
+console.log(square);
+\
+let number = 4;
+
+let cube = number * number * number;
+
+console.log(cube);
+
+let base = 2;
+let exponent = 5;
+
+let result = base ** exponent;
+
+console.log(result);
