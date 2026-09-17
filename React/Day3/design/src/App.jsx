f012dad0f21561e8617ed87cb8fa2c91a46871ab
unknown <you@example.com>
2026-09-17 15:32:50 +0530

    Updated

---
+++ React/Day3/design/src/App.jsx
@@ -0,0 +1,16 @@
+
+const App = ()=>{
+  return(
+  <> 
+  
+  <div>
+    <a href="">Home</a>
+    <a href="">About</a>
+    <a href="">Contact</a>
+    <a href="">Help</a>
+  </div>
+
+  </>
+  )
+}
+export default App()
